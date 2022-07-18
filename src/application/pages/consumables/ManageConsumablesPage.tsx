79d484d80e25d9f729e25b6a5cb560e998da6f80
Vladimir Karsanov <vk@kalite.ro>
2022-07-18 17:59:23 +0300

    Рефакторинг

--- src/application/pages/consumables/ManageConsumablesPage.tsx
+++ src/application/pages/consumables/ManageConsumablesPage.tsx
@@ -1,13 +1,7 @@
 import '../../../library/appearance/layouts/BasicAppLayout/pages.css';
 import '../../../library/appearance/themes/common/size.css'
 
-import React, {useEffect, useState, FunctionComponent, useReducer} from 'react';
-import Domain from "../../domain/Domain";
-import FinancialTransaction from "../../domain/enterprises/FinancialTransaction";
-import DataSet from "../../../library/data/dataSet/DataSet";
-import TableConfig from "../../../library/widgets/tables/dataSetTable/TableConfig";
-import Repository from "../../../library/data/backend/Repository";
-import {DataSetTable} from "../../../library/widgets/tables/dataSetTable/DataSetTable";
+import React, {useState, FunctionComponent} from 'react';
 import ConsumableType from "../../domain/consumables/ConsumableType";
 import {ChooseConsumableTypeWidget} from "./ChooseConsumableTypeWidget";
 import DataObject from "../../../library/data/dataObject/DataObject";
